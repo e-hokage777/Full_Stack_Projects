e706f46ff937aa8d54d2231aefec8168175a34e0
e-hokage777 <e.hokage777@gmail.com>
2022-11-26 19:13:33 +0000

    fixed files not deleting on account deletion [My Gallery]

--- My Gallery/authenticate/delete_account.php
+++ My Gallery/authenticate/delete_account.php
@@ -2,24 +2,43 @@
 session_start();
 
 // importing necessary files
-$root_dir = $_SERVER["DOCUMENT_ROOT"]. "/projects/My Gallery";
+$root_dir = $_SERVER["DOCUMENT_ROOT"] . "/projects/My Gallery";
 require_once($root_dir . "/core/general_functions.php");
 require_once($root_dir . "/classes/user_db_handle.php");
 
-if(isset($_POST["csrf_token"]) && validateCsrfToken($_POST["csrf_token"])){
+if (isset($_POST["csrf_token"]) && validateCsrfToken($_POST["csrf_token"])) {
     $user_db_handle = new user_db_handle();
 
-    // deleting user
-    $isDeleted = $user_db_handle->deleteAccount($_SESSION["userId"]);
+    // delete all files and folder of user in uploads
+    $files = $user_db_handle->get("SELECT location, name FROM uploads WHERE user = ?", "i", $_SESSION["userId"]);
+    $location = "";
 
-    if($isDeleted){
-        $_SESSION = array();
-        echo 0;
+    while ($file = $files->fetch_assoc()) {
+        $location = $file["location"];
+
+        $filename = $location . "/" . $file["name"];
+
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
     }
-    else{
-        echo 2; // unable to delete account and user data for some reason
+
+    // delete the folder
+    if ($location && is_dir($location)) {
+        if (rmdir($location)) {
+            // deleting user
+            $isDeleted = $user_db_handle->deleteAccount($_SESSION["userId"]);
+
+            if ($isDeleted) {
+                $_SESSION = array();
+                echo 0;
+            } else {
+                echo 3; // unable to delete account and user data for some reason
+            }
+        } else {
+            echo 2; // unable to delete directory
+        }
     }
-}
-else{
+} else {
     echo 1; /// invalid or no csrf token
 }
